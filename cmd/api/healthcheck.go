9a43e7f7d922cf490e142be33379345b154067e1
dtdao <kwalitycontrol@gmail.com>
2023-09-07 18:02:55 +0900

    Enveloping response

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,12 +1,27 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healtcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environemtn %s\n", app.config.env)
-	fmt.Fprintf(w, "version %s\n", version)
+
+	systemInfo := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	data := envelope{
+		"status":      "available",
+		"system_info": systemInfo,
+	}
+
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+
+	if err != nil {
+		app.logger.Print(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
 }
--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,11 +1,10 @@
 package main
 
 import (
+	"dtdao/greenlight/internal/data"
 	"fmt"
 	"net/http"
-	"strconv"
-
-	"github.com/julienschmidt/httprouter"
+	"time"
 )
 
 func (app *application) createMovie(w http.ResponseWriter, r *http.Request) {
@@ -13,13 +12,25 @@ func (app *application) createMovie(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-	params := httprouter.ParamsFromContext(r.Context())
-
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	if err != nil || id < 1 {
+	id, err := app.readIDParam(r)
+	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
 
-	fmt.Fprintln(w, "show the details for movie %d\n", id)
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+
+	if err != nil {
+		app.logger.Print(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
