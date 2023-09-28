f503cb72c7cffffc468f2b239e741516dbc701ec
dtdao <kwalitycontrol@gmail.com>
2023-09-28 18:31:23 +0900

    wip authentication endpoint

---
+++ cmd/api/tokens.go
@@ -0,0 +1,12 @@
+package main
+
+import "net/http"
+
+func (app *application) createAuthenticationTokenHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	err := app.readJSON(w, r, &input)
+}
